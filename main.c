d06bb41e19aed3d4e6b5218bd930d8a04aaf0132
OneOfDiscoverer <pm54@ngs.ru>
2023-05-02 23:29:40 +0700

    fix checkNum

--- main.c
+++ main.c
@@ -1,9 +1,37 @@
 #include "main.h"
 
+void printUser(book *pb, int num){
+    printf("ID: %d\nFn: %s \nSn: %s\nPh: %s\nAd: %s\n\n", num, pb[num].firstName, pb[num].secondName, pb[num].phoneNumber, pb[num].address);
+}
+
+void unitClear(book *pb){
+    for(int i = 0; i < sizeof(book); i++){
+        *((char*)pb + i) = 0;
+    }
+}
+
+int checkNum(char* str){
+    const char *end = str + sizeof(str);
+    if(!*str) 
+        return 0;
+    while(str < end){
+        if((*str < '0') || (*str > '9'))
+            return 0;
+        str++;
+        if(!*str) 
+            break;
+    }
+    return 1;
+}
+
 char getCmd(command* cmd){
     int i = 0;
+    int const size = sizeof(command);
     char* ptr = (char*)cmd;
-    while((i < STR_LEN) && ((ptr + i) < ((char*)cmd + sizeof(command)))){
+    for(int i = 0; i < size; i++){
+        *((char*)cmd + i) = 0;
+    }
+    while((i < STR_LEN) && ((ptr + i) < ((char*)cmd + size))){
         char tmp = (char)fgetc(stdin);
         if(tmp == '\n') 
             break;
@@ -18,50 +46,48 @@ char getCmd(command* cmd){
     return *(char*)cmd; 
 }
 
-void printUser(book *pb, int num){
-    printf("ID: %d\nFn: %s \nSn: %s\nPh: %s\nAd: %s\n\n", num, pb[num].firstName, pb[num].secondName, pb[num].phoneNumber, pb[num].address);
-}
-
-void cmdClear(command* cmd){
-    for(int i = 0; i < sizeof(command); i++){
-        *((char*)cmd + i) = 0;
-    }
-}
-
-void unitClear(book *pb){
-    for(int i = 0; i < sizeof(book); i++){
-        *((char*)pb + i) = 0;
-    }
-}
-
 int main(){
     command cmd;
     book phonebook[BOOK_LEN];
 
-    cmdClear(&cmd);
     for(int i = 0; i < BOOK_LEN; i++){
         unitClear(&phonebook[i]);
     }
 
     while(1){
         switch(getCmd(&cmd)){
-            case 'w':{
-                int tmp = atoi(cmd.param1);
-                if(tmp < BOOK_LEN){
-                    char* ptr_cmd = (char*)&cmd.bk;
-                    char* ptr_book = (char*)&phonebook[tmp];
-                    while(ptr_book < ((char*)&phonebook[tmp] + sizeof(book))){
-                        if(!*ptr_book) 
-                            *ptr_book = *ptr_cmd;
-                        ptr_book++;
-                        ptr_cmd++;
+            case 'w':
+                if(checkNum(cmd.param1)){
+                    int tmp = atoi(cmd.param1);
+                    if(tmp < BOOK_LEN){
+                        char* ptr_cmd = (char*)&cmd.bk;
+                        char* ptr_book = (char*)&phonebook[tmp];
+                        while(ptr_book < ((char*)&phonebook[tmp] + sizeof(book))){
+                            if(!*ptr_book) 
+                                *ptr_book = *ptr_cmd;
+                            ptr_book++;
+                            ptr_cmd++;
+                        }
                     }
+                    else 
+                        printf("out of range\n");
                 }
                 break;
-            }    
+            case 'r':
+                if(checkNum(cmd.param1)){
+                    int tmp = atoi(cmd.param1);
+                    if(tmp < BOOK_LEN)
+                        printUser(phonebook, tmp);
+                    else 
+                        printf("out of range\n");
+                }
+                break;
             case 'd':
-                if(atoi(cmd.param1) < BOOK_LEN) 
-                    unitClear(&phonebook[atoi(cmd.param1)]);
+                if(checkNum(cmd.param1)){
+                    int tmp = atoi(cmd.param1);
+                    if(tmp < BOOK_LEN) 
+                        unitClear(&phonebook[tmp]);
+                }
                 break;
             case 'l':
                 for(int i = 0; i < BOOK_LEN; i++){
@@ -69,15 +95,17 @@ int main(){
                 }
                 break;
             case 's':
-                for(int i = 0; i < BOOK_LEN; i++){
-                    for(int b = 0; b < sizeof(book)/STR_LEN; b++){
-                        int result = 0;
-                        char* ptr = (char*)&phonebook[i] + STR_LEN*b;
-                        for(int j = 0; j < STR_LEN; j++){
-                            result += *(cmd.param1+j) - *(ptr+j);
+                if(*cmd.param1){
+                    for(int i = 0; i < BOOK_LEN; i++){
+                        for(int b = 0; b < sizeof(book)/STR_LEN; b++){
+                            int result = 0;
+                            char* ptr = (char*)&phonebook[i] + STR_LEN*b;
+                            for(int j = 0; j < STR_LEN; j++){
+                                result += *(cmd.param1+j) - *(ptr+j);
+                            }
+                            if(!result)
+                                printUser(phonebook, i);
                         }
-                        if(!result) 
-                            printUser(phonebook, i);
                     }
                 }
                 break;
@@ -85,10 +113,9 @@ int main(){
                 return 0;
                 break;
             default:
-                printf("help\nw - write [FirstName] [SecondName] [PhoneNumber] ...\nd - delene [ID]\nl - list all\ns - search [param]\nq - quit\n");
+                printf("help\nw - write [FirstName] [SecondName] [PhoneNumber] ...\nr - read [ID]\nd - delene [ID]\nl - list all\ns - search [PARAM]\nq - quit\n");
                 break;
         }
-        cmdClear(&cmd);
     }
     return 0;
 }
