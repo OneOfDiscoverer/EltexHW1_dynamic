50df35a0e33a8954e2bb90c1997ad8ddf67ce1c2
OneOfDiscoverer <pm54@ngs.ru>
2023-05-02 17:45:15 +0700

    moving defines

--- main.c
+++ main.c
@@ -1,11 +1,5 @@
 #include "main.h"
 
-//для изменения количества полей необходимо менять структуру book и вывод в функции PrintUser.
-//Длина книги и длина строки в структуре изменяется макросами ниже.
-
-#define BOOK_LEN 10
-#define STR_LEN 16
-
 struct book 
 {
     char firstName[STR_LEN];
